d1448f55d5fa436b4c98444d61c02d95fd86e8e4
advmaster <118003523+advmaster@users.noreply.github.com>
2023-09-23 19:36:27 -0300

    a

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container py-5">
+      <div className="row justify-content-center">
+        <div className="col-lg-4">
+
+        <div className="card" >
+          <img className="card-img-top" src="https://st2.depositphotos.com/1692343/5636/i/450/depositphotos_56360353-stock-photo-hot-homemade-pepperoni-pizza.jpg" alt="Imagem de capa do card" />
+          <div className="card-body">
+            <h5 className="card-title">Título do card</h5>
+            <p className="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+            <a href="#" className="btn btn-primary">Visitar</a>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
