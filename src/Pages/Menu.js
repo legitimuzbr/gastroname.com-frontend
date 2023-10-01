30623eb699800cc61b5a8022d0ea2a76a62738de
advmaster <118003523+advmaster@users.noreply.github.com>
2023-09-30 23:18:54 -0300

    a

---
+++ src/Pages/Menu.js
@@ -0,0 +1,13 @@
+import Navbar from "../Components/Navbar";
+import Cardapio from "../Components/Cardapio";
+
+export default function Menu() {
+
+  return (
+    <>
+      <Navbar />
+      <Cardapio />
+    </>
+  );
+
+}
