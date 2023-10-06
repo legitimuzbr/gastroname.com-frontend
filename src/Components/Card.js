1cf6e120a64ebbe87631a4776029776563aed68c
legitimuzbr <141097352+legitimuzbr@users.noreply.github.com>
2023-10-06 23:10:07 +0000

    a

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,11 +1,25 @@
 export default function Card(props) {
   return (
-    <div className="card mb-3" style={{ width: "18rem;" }}>
-      <img src={props.image} className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
-        <p className="card-text">{props.description}</p>
-        <span class="badge rounded-pill text-bg-success">R$ {props.price}</span>
+    <div className="card mb-3" style={{ maxWidth: "540px" }}>
+      <div className="row g-0 align-items-center">
+        <div className="col-4">
+          <img
+            src={props.image}
+            className="img-fluid rounded-start"
+            alt="..."
+          />
+        </div>
+        <div className="col-8">
+          <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.description}</p>
+            <p className="card-text">
+              <span class="badge rounded-pill text-bg-success">
+                R$ {props.price}
+              </span>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Items.js
+++ src/Pages/Items.js
@@ -7,7 +7,7 @@ const Items = ({ items, setSelectedCategory }) => {
       <div className="container">
         <div className="row">
           <button
-            className="btn btn-secondary mb-3"
+            className="btn btn-success mb-3"
             onClick={() => setSelectedCategory(null)}
           >
             Voltar
@@ -24,7 +24,7 @@ const Items = ({ items, setSelectedCategory }) => {
             </button>
           </div>
           {items.map((item, index) => (
-            <div className="col-6 col-md-4 col-lg-3">
+            <div className="col-12 col-md-8">
               <Card
                 key={index}
                 title={item.title}
