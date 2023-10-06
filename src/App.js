5048f42ffa42b4c5da707725300f08ec293b5518
legitimuzbr <141097352+legitimuzbr@users.noreply.github.com>
2023-10-06 22:12:44 +0000

    b

--- src/App.js
+++ src/App.js
@@ -8,14 +8,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          index
-          element={
-            <Link to="/1" preventScrollReset={true}>
-              Gastroname
-            </Link>
-          }
-        />
+        <Route index element={<h1>Gastroname</h1>} />
         <Route path="/:userId" element={<Menu />} />
       </Routes>
     </BrowserRouter>
